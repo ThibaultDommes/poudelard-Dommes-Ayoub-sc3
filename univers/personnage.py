f3846ea4532a7bbca97648d78e4541ff41383302
itzkayt0zz <sam.ayoub@efrei.net>
2025-12-05 10:06:53 +0100

    lol

--- univers/personnage.py
+++ univers/personnage.py
@@ -1,2 +1,23 @@
+def initialiser_personnage(nom, prenom, attributs):
+    joueur = {
+        "Nom": nom,
+        "Prenom": prenom,
+        "Argent": 100,
+        "Inventaire": [],
+        "Sortilèges": [],
+        "Attributs": attributs
+    }
+    return joueur
+
+
+def afficher_personnage(joueur):
+    print("Profil du personnage :")
+    for cle in joueur:
+
+        valeur = joueur[cle]
+        if type(valeur) == dict:
+            print(cle, ":")
+            for sous_cle in valeur:
+                print(" -", sous_cle, ":", valeur[sous_cle])
 
 
