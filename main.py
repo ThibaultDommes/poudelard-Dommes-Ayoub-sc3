4267880603a0ed7301e10c3a819e529e8f7e74bb
ThibaultDommes <thibault.dommes@efrei.net>
2025-12-05 10:04:29 +0100

    fonction demandertexte

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-hahaha
+from utils.input_utils import *
+
+if __name__ == '__main__':
+    message = input("Entrez le nom de votre personnage : ")
+    demander_texte(message)
+    print(message)
+    demander_choix(message,1,10)
---
+++ utils/input_utils.py
@@ -0,0 +1,12 @@
+def demander_texte(message):
+    while message == "":
+        message = input("Entrez le nom de votre personnage : ")
+    else :
+        return message
+
+def demander_choix(message,min_val,max_val):
+    while max_val < a or min_val > b:
+        a = int(input("Veuillez entrer un nombre entre ",min_val," et ",max_val,"."))
+    else :
+        return message
+
