6014babc15e7e4f273f911f793e76a201d244bb6
itzkayt0zz <sam.ayoub@efrei.net>
2025-12-12 11:17:48 +0100

    main

--- main.py
+++
@@ -1,7 +0,0 @@
-from utils.input_utils import *
-
-if __name__ == '__main__':
-    message = input("Entrez le nom de votre personnage : ")
-    demander_texte(message)
-    print(message)
-    demander_choix(message,1,10)
