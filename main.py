32b980bfaca8278b977d17efe176d9a710d4226d
ThibaultDommes <thibault.dommes@efrei.net>
2025-12-12 11:14:14 +0100

    utils

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from utils.input_utils import *
 if __name__ == '__main__':
     message = input("Entrez le nom de votre personnage : ")
     demander_texte(message)
-    print(message)
-    demander_choix(message,1,10)
+    demander_nombre("Niveau de courage",1,10)
+    demander_choix("Voulez-vous continuer ?", ["Oui", "Non"])
