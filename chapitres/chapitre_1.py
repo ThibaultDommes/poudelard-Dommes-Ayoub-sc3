a87f28a5c01f26955517ee6fb931506f21a952b0
ThibaultDommes <thibault.dommes@efrei.net>
2025-12-21 14:51:52 +0100

    CREER PERSONNAGE

--- chapitres/chapitre_1.py
+++ chapitres/chapitre_1.py
@@ -1,91 +1,45 @@
-from utils.input_utils import demander_texte, demander_nombre, load_fichier
-from univers.personnage import initialiser_personnage, afficher_personnage, modifier_argent, ajouter_objet
-from univers.maison import initialiser_maisons
+from utils.input_utils import *
+from univers.personnage import *
+from univers.maison import *
 
-def lancer_chapitre_1():
-    print("===== Chapitre 1 : La lettre de Poudlard =====")
+def introduction():
+    print("Bienvenue dans le monde magique de Poudlard.")
+    print("Votre aventure commence aujourd'hui.")
+    print("Une lettre très particulière va bientôt changer votre destin...")
+    input("Appuyez sur Entrée pour continuer.")
+
+def creer_personnage():
     nom = demander_texte("Entrez le nom de votre personnage : ")
     prenom = demander_texte("Entrez le prénom de votre personnage : ")
 
-    print("Définissez les attributs de votre personnage (0 à 10).")
-    courage = demander_nombre("Courage : ", 0, 10)
-    loyaute = demander_nombre("Loyauté : ", 0, 10)
-    ambition = demander_nombre("Ambition : ", 0, 10)
-    intelligence = demander_nombre("Intelligence : ", 0, 10)
+    print("Choisissez vos attributs :")
+    courage = demander_nombre("Niveau de courage : ",1,10)
+    intelligence = demander_nombre("Niveau d’intelligence : ",1,10)
+    loyaute = demander_nombre("Niveau de loyauté : ",1,10)
+    ambition = demander_nombre("Niveau d’ambition  : ",1,10)
 
     attributs = {
         "courage": courage,
-        "loyaute": loyaute,
-        "ambition": ambition,
-        "intelligence": intelligence
+        "intelligence": intelligence,
+        "loyauté": loyaute,
+        "ambition": ambition
     }
 
-    joueur = initialiser_personnage(nom, prenom, attributs)
-    afficher_personnage(joueur)
+    personnage = initialiser_personnage(nom, prenom, attributs)
 
-    print()
-    print("Un hibou frappe à ta fenêtre...")
-    print("Tu reçois ta lettre d'admission à l'école de sorcellerie !")
-    print("Puis Hagrid arrive pour t'emmener au Chemin de Traverse acheter tes fournitures.")
-    print()
+    print("Profil du personnage :")
+    print("Nom :", personnage["Nom"])
+    print("Prenom :", personnage["Prenom"])
+    print("Argent :", personnage["Argent"])
 
-    try:
-        inventaire_data = load_fichier("data/inventaire.json")
-    except FileNotFoundError:
-        print("Fichier inventaire.json introuvable, aucun achat possible.")
-        inventaire_data = []
+    print("Inventaire :")
+    print(personnage["Inventaire"])
 
-    if isinstance(inventaire_data, dict):
-        objets_disponibles = inventaire_data.get("objets", [])
-    else:
-        objets_disponibles = inventaire_data
+    print("Sortilèges :")
+    print(personnage["Sortilèges"])
 
-    print("Tu disposes de", joueur["Argent"], "pièces d'or pour acheter du matériel.")
+    print("Attributs :")
+    for cle, valeur in personnage["Attributs"].items():
+        print("-", cle, ":", valeur)
 
-    terminer = False
-    while not terminer and len(objets_disponibles) > 0:
-        print()
-        print("Objets disponibles :")
-        i = 0
-        while i < len(objets_disponibles):
-            objet = objets_disponibles[i]
-            nom_obj = objet["nom"]
-            prix = objet["prix"]
-            print(str(i + 1) + ".", nom_obj, "-", prix, "pièces")
-            i = i + 1
-        print("0. Terminer les achats")
-
-        choix = demander_nombre("Que veux-tu acheter ? (numéro) : ", 0, len(objets_disponibles))
-
-        if choix == 0:
-            terminer = True
-        else:
-            index = choix - 1
-            objet = objets_disponibles[index]
-            nom_obj = objet["nom"]
-            prix = objet["prix"]
-
-            if joueur["Argent"] >= prix:
-                modifier_argent(joueur, -prix)
-                ajouter_objet(joueur, "Inventaire", nom_obj)
-                print("Tu as acheté", nom_obj, ". Argent restant :", joueur["Argent"])
-            else:
-                print("Tu n'as pas assez d'argent pour acheter", nom_obj)
-
-    print()
-    print("Fin du chapitre 1. Tu es prêt pour le voyage vers Poudlard !")
-    afficher_personnage(joueur)
-
-    try:
-        maisons_data = load_fichier("data/maisons.json")
-    except FileNotFoundError:
-        maisons_data = ["Gryffondor", "Serpentard", "Poufsouffle", "Serdaigle"]
-
-    if isinstance(maisons_data, dict):
-        noms_maisons = list(maisons_data.keys())
-    else:
-        noms_maisons = maisons_data
-
-    maisons = initialiser_maisons(noms_maisons)
-
-    return joueur, maisons
+    return personnage
